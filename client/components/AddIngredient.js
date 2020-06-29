e8825b21f083cfecd9d581f1ba22f1cf978acd04
Sarah Park <sarahyaepark@gmail.com>
2020-06-28 18:52:36 -0700

    Feature: Add to fridge

--- client/components/AddIngredient.js
+++ client/components/AddIngredient.js
@@ -2,45 +2,25 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { connect } from "react-redux";
-import { fetchIngredients } from "../store/ingredient";
 
-export class AddIngredient extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log(this.props);
-  }
-  handleSubmit(event) {
-    event.preventDefault();
-    let ingredientName = event.target.ingredientName.value;
-    let id = this.props.id;
-    // call thunk
-  }
-  render() {
-    return (
-      <div>
-        <h3>What's in your fridge?</h3>
-        <Form>
-          <Form.Group controlId="formInput">
-            <Form.Label>Ingredient</Form.Label>
-            <Form.Control
-              type="text"
-              name="ingredientName"
-              placeholder="Tomato..."
-            />
-          </Form.Group>
-          <Button id="tealButton" size="lg" type="submit">
-            Add Ingredient
-          </Button>
-        </Form>
-      </div>
-    );
-  }
-}
-
-const mapState = (state) => {
-  return {
-    id: state.user.id,
-  };
+export const AddIngredient = (props) => {
+    const {handleSubmit} = props
+  return (
+    <div>
+      <h3>What's in your fridge?</h3>
+      <Form onSubmit ={handleSubmit}>
+        <Form.Group controlId="formInput">
+          <Form.Label>Ingredient</Form.Label>
+          <Form.Control
+            type="text"
+            name="ingredientName"
+            placeholder="Tomato..."
+          />
+        </Form.Group>
+        <Button id="tealButton" size="lg" type="submit">
+          Add Ingredient
+        </Button>
+      </Form>
+    </div>
+  );
 };
-
-export default connect(mapState)(AddIngredient);
